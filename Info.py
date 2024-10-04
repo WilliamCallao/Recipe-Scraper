e956e7ab26124617447a03d4129fae96f107f13f
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-04 18:40:12 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,28 +1,31 @@
 import os
 import glob
+import csv
 from collections import Counter
 
-# Obtener la ruta actual donde se está ejecutando el script
 current_directory = os.getcwd()
 
-# Buscar todos los archivos CSV que comienzan con "Urls- " en la ruta actual
-csv_files = glob.glob(os.path.join(current_directory, "Urls-*.csv"))
+csv_files = glob.glob(os.path.join(current_directory, "Urls -*.csv"))
 
-# Contador para llevar la cuenta de cuántos archivos hay con el mismo prefijo
 file_counter = Counter()
 
-# Contar los archivos
+total_records = 0
+
 for file in csv_files:
-    # Extraer el prefijo sin la extensión para contar cuántos hay de cada uno
     file_name = os.path.basename(file)
     prefix = file_name.split(".csv")[0]
     file_counter[prefix] += 1
 
-# Imprimir el conteo de cada tipo de archivo
+    with open(file, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        record_count = sum(1 for row in reader)
+        total_records += record_count
+
 print("Cantidad de archivos por nombre:")
 for prefix, count in file_counter.items():
     print(f"{prefix}: {count}")
 
-# Imprimir el total de archivos CSV encontrados
 total_files = len(csv_files)
 print(f"\nTotal de archivos CSV encontrados: {total_files}")
+
+print(f"Total de registros en todos los archivos CSV: {total_records}")
